9b1d682d7e9fc592273283f5a1eee58aae426c09
Wahid Ur Rehman <wahid.rehman.dev@gmail.com>
2026-03-13 00:47:36 +0500

    chore: v1.0.5 - build Mac x64 + arm64, landing page updates

--- src/renderer/vite-env.d.ts
+++ src/renderer/vite-env.d.ts
@@ -1,5 +1,8 @@
 /// <reference types="vite/client" />
 
+declare const __APP_VERSION__: string;
+declare const __APP_HOMEPAGE__: string;
+
 declare module '*.svg' {
   const content: string;
   export default content;
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
+import packageJson from './package.json';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  define: {
+    __APP_VERSION__: JSON.stringify(packageJson.version),
+    __APP_HOMEPAGE__: JSON.stringify(packageJson.homepage),
+  },
   root: path.resolve(__dirname, './src/renderer'),
   plugins: [react()],
   base: './',
